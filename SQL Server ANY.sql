f5d4d463a66cf8bac395cd16e07f572675df3087
Krasimir Garev <kgarev@gmail.com>
2026-08-03 08:11:07 +0300

    Add files via upload

---
+++ SQL Server ANY.sql
@@ -0,0 +1,13 @@
+SELECT
+    product_name,
+    list_price
+FROM production.products
+WHERE product_id = ANY (
+        SELECT  
+            product_id
+        FROM sales.order_items
+        WHERE 
+        quantity >= 2
+    )
+ORDER BY product_name;
+GO
